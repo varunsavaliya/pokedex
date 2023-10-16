d3cbe815ab53c41a29a2fd804e95609610009aab
Mit-Bhayani-2610 <varunsavaliya000@gmail.com>
2023-10-16 15:31:16 +0530

    reduced the states and combined all states in one state

--- src/components/PokemonList/PokemonList.jsx
+++ src/components/PokemonList/PokemonList.jsx
@@ -4,57 +4,82 @@ import "./PokemonList.css";
 import Pokemon from "../Pokemon/Pokemon";
 
 function PokemonList() {
-  const [pokemonList, setPokemonList] = useState([])
-  const [isLoading, setIsLoading] = useState(true)
-  const [prev, setPrev] = useState('')
-  const [next, setNext] = useState('')
-  const [pokedexUrl, setPokedexUrl] = useState('https://pokeapi.co/api/v2/pokemon')
+  //   const [pokemonList, setPokemonList] = useState([])
+  //   const [isLoading, setIsLoading] = useState(true)
+  //   const [prev, setPrev] = useState('')
+  //   const [next, setNext] = useState('')
+  //   const [pokedexUrl, setPokedexUrl] = useState('https://pokeapi.co/api/v2/pokemon')
+  const [pokedexState, setPokedexState] = useState({
+    pokemonList: [],
+    isLoading: true,
+    prev: "",
+    next: "",
+    pokedexUrl: "https://pokeapi.co/api/v2/pokemon",
+  });
 
   async function getPokemons() {
-    setIsLoading(true)
-    const response = await axios.get(pokedexUrl); // get data from api
-    setPrev(response.data.previous)
-    setNext(response.data.next)
+    setPokedexState((state) => ({ ...state, isLoading: true }));
+    const response = await axios.get(pokedexState.pokedexUrl); // get data from api
+    setPokedexState((state) => ({ ...state, prev: response.data.previous, next: response.data.next}));
 
     // result have 20 pokemons name and url
-    const pokemonResults = response.data.results
+    const pokemonResults = response.data.results;
 
     // promise array of 20 pokemons
-    const pokemonResultsPromise = pokemonResults.map((pokemon) => axios.get(pokemon.url))
-    const pokemonData = await axios.all(pokemonResultsPromise)
+    const pokemonResultsPromise = pokemonResults.map((pokemon) =>
+      axios.get(pokemon.url)
+    );
+    const pokemonData = await axios.all(pokemonResultsPromise);
 
     // mapping of 20 pokemons data
     const result = pokemonData.map((p) => {
-      const pokemon = p.data
+      const pokemon = p.data;
       return {
         id: pokemon.id,
         name: pokemon.name,
         image: pokemon.sprites.other.dream_world.front_default,
-        types: pokemon.types
-      }
-    })
-    setPokemonList(result)
-    setIsLoading(false)
+        types: pokemon.types,
+      };
+    });
+    setPokedexState((state) => ({
+      ...state,
+      pokemonList: result,
+      isLoading: false,
+    }));
   }
 
   useEffect(() => {
     getPokemons();
-  }, [pokedexUrl]);
+  }, [pokedexState.pokedexUrl]);
   return (
     <>
       <div className="pokemon-list-wrapper row g-3 my-3">
-        {(isLoading) ? 'Loading....' :
+        {pokedexState.isLoading ? (
+          "Loading...."
+        ) : (
           <>
-            {pokemonList.map((p) => (
+            {pokedexState.pokemonList.map((p) => (
               <Pokemon name={p.name} image={p.image} id={p.id} key={p.id} />
             ))}
 
             <div className="controls d-flex justify-content-center">
-              <button className="btn btn-primary mx-2" disabled={prev == null} onClick={() => setPokedexUrl(prev)}>Previous</button>
-              <button className="btn btn-primary mx-2" disabled={next == null} onClick={() => setPokedexUrl(next)}>Next</button>
+              <button
+                className="btn btn-primary mx-2"
+                disabled={pokedexState.prev == null}
+                onClick={() => setPokedexState((state)=>({...state, prev: pokedexState.prev}))}
+              >
+                Previous
+              </button>
+              <button
+                className="btn btn-primary mx-2"
+                disabled={pokedexState.next == null}
+                onClick={() => setPokedexState((state)=>({...state, next: pokedexState.next}))}
+              >
+                Next
+              </button>
             </div>
           </>
-        }
+        )}
       </div>
     </>
   );
